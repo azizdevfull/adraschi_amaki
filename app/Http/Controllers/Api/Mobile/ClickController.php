1743bb5e0bd86e2ea19a6598eb531b4918df4d96
Azizbek Isroilov <aziz16110904@gmail.com>
2023-09-04 11:51:33 +0500

    changed order many to many

--- app/Http/Controllers/Api/Mobile/ClickController.php
+++ app/Http/Controllers/Api/Mobile/ClickController.php
@@ -90,16 +90,16 @@ class ClickController extends Controller
         if ($error == 0) {
             ClickUz::where('click_trans_id', $clickTransId)->update(['situation' => 1, 'status' => 'success']);
             $order = Order::where('id', $merchantTransId)->update([ 'status' => 'success']); // Retrieve the Order
-            // if ($order) {
+            if ($order) {
             //     $order->update(['status' => 'yakunlandi']);
             
-            //     $adminUsers = User::where('role', 1)->get();
+                $adminUsers = User::where('role', 1)->get();
             
-            //     foreach ($adminUsers as $admin) {
-            //         $admin->notify(new NewOrderNotification($order)); // Notify each admin with the Order instance
-            //     }
+                foreach ($adminUsers as $admin) {
+                    $admin->notify(new NewOrderNotification($order)); // Notify each admin with the Order instance
+                }
             
-            // }
+            }
             return response()->json([
                 'click_trans_id' => $clickTransId,
                 'merchant_trans_id' => $merchantTransId,
